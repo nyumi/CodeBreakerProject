eeabdadf0c4de1f51fe1a3818a50dcbeb3cf3eeb
yum <yum@yumnoMacBook-Pro.local>
2017-02-04 22:03:44 +0900

    🐷

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,12 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+  
 }
 
 //implement new functions here
+function setHiddenFields() {
+    const MAX = 9999;
+    const MIN = 0;
+    let answer = Math.floor( Math.random() * (MAX - MIN + 1) ) + MIN;
+}
